90a1270042df2397994925234f57bf7042377e24
Ilia <63251213+galietskii@users.noreply.github.com>
2021-01-12 13:32:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 function t1() {
    let a = document.querySelector('.text-edit');
     a.setAttribute('contentEditable', 'true');
+    let items = document.getElementsByClassName("text-edit");
+    for (let i = 0; i < items.length; i++) {
+  items[i].setAttribute("contentEditable", 'true');
 }
-    
+}
+
 document.querySelector('.square').onclick = t1;
 function t2() {
     let a = document.querySelector('.text-edit');
-     a.setAttribute('contentEditable', 'false');
+    a.setAttribute('contentEditable', 'false');
+    let items = document.getElementsByClassName("text-edit");
+    for (let i = 0; i < items.length; i++) {
+  items[i].setAttribute("contentEditable", 'false');
  }
-     
+ }
  document.querySelector('.circle').onclick = t2;
